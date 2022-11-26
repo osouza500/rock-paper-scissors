c9d22113293c4aa8e09362b7f4db1fb8a7f0135e
osouza <mauricio.souza002@gmail.com>
2022-11-26 09:44:58 -0200

    Formatar código de acordo com pycodestyle

--- rps-starter-code.py
+++ rps-starter-code.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
-
-"""This program plays a game of Rock, Paper, Scissors between two Players,
-and reports both Player's scores each round."""
-
-moves = ['rock', 'paper', 'scissors']
-
-"""The Player class is the parent class for all of the Players
-in this game"""
-
 import random
+import time
+
 
 class Player:
     def move(self):
@@ -18,38 +10,41 @@ class Player:
     def learn(self, my_move, their_move):
         pass
 
+
 class RandomPlayer(Player):
     pass
+
 
 class HumanPlayer(Player):
     def move(self):
         result = input('Rock, paper or scissors?\n')
         return result
 
+
 class ReflectPlayer(Player):
-    def learn(self, my_move, their_move):  
+    def learn(self, my_move, their_move):
         p1_move = their_move
         return p1_move
 
+
 class CyclePlayer(Player):
-    def learn(self, my_move):
+    def learn(self, my_move, their_move):
         result = ['rock', 'paper', 'scissors']
         if my_move in result:
             result.remove(my_move)
             return random.choice(result)
-        
-        
+
 
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
-            
+
 
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
-        self.p2 = p2 
+        self.p2 = p2
 
     def play_game(self):
         p1_score = 0
@@ -61,23 +56,26 @@ class Game:
             if round == 0:
                 move2 = self.p2.move()
             print(f"Player 1: {move1}  Player 2: {move2}")
-            if beats(move1, move2) == True:
+            if beats(move1, move2) is True:
                 p1_score += 1
                 print("Player One won.")
-                print(f"Score = Player One: {p1_score}, Player Two: {p2_score}.")
-            elif beats(move2, move1) == True:
+                print(f"Score = Player One: {p1_score}, "
+                      f"Player Two: {p2_score}.")
+            elif beats(move2, move1) is True:
                 p2_score += 1
                 print("Player Two won.")
-                print(f"Score = Player One: {p1_score}, Player Two: {p2_score}.")
+                print(f"Score = Player One: {p1_score}, "
+                      f"Player Two: {p2_score}.")
             elif move1 == move2:
                 print("Tie!")
-                print(f"Score = Player One: {p1_score}, Player Two: {p2_score}.")
-            move2 = self.p2.learn(move2, move1)   
+                print(f"Score = Player One: {p1_score}, "
+                      f"Player Two: {p2_score}.")
+            move2 = self.p2.learn(move2, move1)
         print("Game over!")
 
 
 if __name__ == '__main__':
-    game = Game(HumanPlayer(), ReflectPlayer())
+    game = Game(HumanPlayer(), CyclePlayer())
     game.play_game()
 
     # def play_round(self, p1_score, p2_score):
